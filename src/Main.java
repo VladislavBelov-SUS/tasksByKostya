06579b6251c384f0c90edd6cd7a2ebfb1e62c151
Vladik-marlmeladik <belo4kalala1441@gmaial.com>
2022-05-29 17:38:19 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter array length: ");
+
+
+        MyArray array = new MyArray();
+        array.filling();
+        array.inputArrayElement();
+        array.eraseArrayElement();
+        array.searchArrayIndex();
+        array.searchArrayElement();
+
+
+    }
+}
---
+++ src/MyArray.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyArray {
+    Scanner input = new Scanner(System.in);
+    int size = input.nextInt();
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    public MyArray() {
+
+    }
+
+    public void filling() {
+        int low = 0;
+        int high = 1000;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt((high - low) + low);
+            System.out.println("Индекс массива[" + i + "]=" + array[i]);
+        }
+    }
+
+    public void inputArrayElement() {
+        System.out.println("Введите число на которое хотите заменить последний элемент Массива -> ");
+        int num = 100;
+        int index = array.length;
+        int[] newArray = new int[array.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[index] = 100;
+        for (int i = index; i < array.length; i++) {
+            newArray[i - 1] = array[i];
+        }
+        for (int i : newArray) {
+            System.out.println("Индекс массива[" + i + "]=" + newArray[i]);
+        }
+    }
+
+    public void eraseArrayElement() {
+        System.out.println("Введите индекс массива, который вы хотите удалить-> ");
+
+        int userInput = input.nextInt();
+        for (int i = userInput; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = 0;
+        for (int i : array) {
+            System.out.println("Индекс массива[" + i + "]=" + array[i]);
+        }
+    }
+
+    public void searchArrayIndex() {
+        System.out.println("Введите индекс массива, чтобы увидеть число-> ");
+        int userInput = input.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            if (userInput == i) {
+                System.out.println("Индекс массива[" + array[i] + "]");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public void searchArrayElement() {
+        System.out.println("Введите число из массива, индекс которого вы хотите узнать-> ");
+
+        int userInput = input.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            if (userInput == array[i]) {
+                System.out.println("Индекс массива[" + i + "]");
+            }
+        }
+
+    }
+
+}
